b5f2bca29395b50136698770c61812077b8aecc6
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-08-04 22:44:29 -0400

    testes

--- src/main/java/br/com/cdi/qualificadores/ControllerEnvioMensagem.java
+++ src/main/java/br/com/cdi/qualificadores/ControllerEnvioMensagem.java
@@ -1,23 +1,39 @@
 package br.com.cdi.qualificadores;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Default;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.cdi.model.Usuario;
+
 
 @Named("envioMensagem")
-@RequestScoped
-public class ControllerEnvioMensagem {
+@SessionScoped
+public class ControllerEnvioMensagem implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	private String texto;
+	private Usuario usuario = new Usuario();
+	
 	
 	@Inject 
 	private Mensageiro mensageiro;
+	private List<Usuario> listaNomes = new ArrayList<Usuario>();
+	
+	
 	
 	public void enviarMensagem() {
 		this.mensageiro.enviarMensagem(texto);
 	}
+	
 	
 	public String getTexto() {
 		return texto;
@@ -25,6 +41,23 @@ public class ControllerEnvioMensagem {
 	
 	public void setTexto(String texto) {
 		this.texto = texto;
+	}
+	
+	
+	public void salvar() {
+		this.listaNomes.add(this.usuario);
+		this.usuario = new Usuario();
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public List<Usuario> getListaNomes() {
+		return listaNomes;
 	}
 	
 }
